bcfc835e131f8ef90d27435e68efbc5a3475f2ab
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2025-04-29 10:12:20 +0530

    first commit

---
+++ src/test/java/icet/hrm/Fortium/Partners/FortiumPartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package icet.hrm.Fortium.Partners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortiumPartnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
